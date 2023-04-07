bbbdc8edf7e6206c5bc3bb2f150c5fa410495c34
League <zach.dantsker@gmail.com>
2023-04-07 16:54:33 -0700

    help.

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -5,6 +5,9 @@ import processing.core.PApplet;
 public class FlappyBird extends PApplet {
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
+    boolean end = intersectsPipes();;
+    int ground = 590;
+    int score = 0;
  int x = 400;
  int y = 300;
  int birdYVelocity =  -1;
@@ -12,7 +15,7 @@ public class FlappyBird extends PApplet {
 int pipeX = 0;
 int pipeY = 0;
 int pipeH = (int) random(100, 400);
-int pipeGap = 100;
+int pipeGap = 300;
 int lowerY = 500;
 
     @Override
@@ -28,18 +31,20 @@ int lowerY = 500;
     @Override
     public void draw() {
     	background(0, 150, 180);
-    	 intersectsPipes();
+    	 
     	fill(10, 150, 0);
     	rect(pipeX, pipeY, 100, pipeH);
+    	rect(0, ground, 800, 10);
     	fill(200, 200, 200);
     	fill(10, 150, 0);
     	rect(pipeX, 500, 100, lowerY);
     	fill(200, 200, 200);
-
     	stroke(200, 200, 200);
     	ellipse(x, y, 20, 20);
     	mousePressed();
-    	pipeX = pipeX + 5;
+    	  
+    	 
+    	pipeX = pipeX + 5; 
     	gravity = gravity - birdYVelocity;
     	y = y + gravity;
     	if(pipeX > HEIGHT) {
@@ -47,8 +52,22 @@ int lowerY = 500;
     		pipeH = (int) random(100, 400);
     		lowerY = pipeH + pipeGap;
     	}
+    	if (y < pipeH &&  x > pipeX && x < (pipeX+100)){
+         end = true; }
+        else if (y>lowerY && x > pipeX && x < (pipeX+100)) {
+            end = true; }
+        else { end = false; score = score + 1;} 
+    	 if( end == true) {
+ 	    	pipeX = 0; 
+ 	    	text(" your score is "+score+".", 400, 300);
+ 	    	
+ 	 }
+     }
+    	
+    
+    	
   
-    }
+    
 
     static public void main(String[] args) {
         PApplet.main(FlappyBird.class.getName());
