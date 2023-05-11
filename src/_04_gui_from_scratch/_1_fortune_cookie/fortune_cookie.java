772cbae474ce5300e8b029a4ca693b838ff6a57e
sabka <sabka@X15.attlocal.net>
2023-05-10 20:57:38 -0700

    JFrames make me wanna kms.

--- src/_04_gui_from_scratch/_1_fortune_cookie/fortune_cookie.java
+++ src/_04_gui_from_scratch/_1_fortune_cookie/fortune_cookie.java
@@ -2,6 +2,7 @@ package _04_gui_from_scratch._1_fortune_cookie;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -26,6 +27,23 @@ public void actionPerformed(ActionEvent e) {
 	JButton buttonPressed = (JButton) e.getSource();	
 	if(buttonPressed == babygirl){
 		JOptionPane.showMessageDialog(null, "Woohoo!!");
+		int rand = new Random().nextInt(5);
+	if(rand == 0) {
+		JOptionPane.showMessageDialog(null, "Oh my, your deppresed!!");
+	}if(rand == 1) {
+		JOptionPane.showMessageDialog(null, "Oh my, your quite lucky!!");
+	}
+	if(rand == 2) {
+		JOptionPane.showMessageDialog(null, "lmao skill issues, your hella skrewed!");
+	}
+	if(rand == 3) {
+		JOptionPane.showMessageDialog(null, "ring ring ring! Harvard is calling (the police)!");
+	}
+	if(rand == 4) {
+		JOptionPane.showMessageDialog(null, "better show mom that report card. it will save your life!");
+	}
+	
+
 	}
 }
 }
