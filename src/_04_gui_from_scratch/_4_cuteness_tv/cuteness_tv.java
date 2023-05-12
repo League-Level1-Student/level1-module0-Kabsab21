bcb073b2ff1efb7fd032a2806c82cd39a02e97a0
league <league@imac-18.attlocal.net>
2023-05-12 16:54:30 -0700

    sdfas

--- src/_04_gui_from_scratch/_4_cuteness_tv/cuteness_tv.java
+++ src/_04_gui_from_scratch/_4_cuteness_tv/cuteness_tv.java
@@ -1,5 +1,50 @@
 package _04_gui_from_scratch._4_cuteness_tv;
 
+import java.awt.event.ActionListener;
+
 public class cuteness_tv {
+	public static void main(String[] args) implements ActionListener {
+		cuteness_tv runner = new cuteness_tv();
+		
+	}
+	
+	
+	private void buttons() {
+		
+	}
+	
+	
+	
+	
+	
+	void showDucks() {
+	     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+
+	void showFrog() {
+	     playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+
+	void showFluffyUnicorns() {
+	     playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
+
+	void playVideo(String videoID) {
+	     
+	     // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't
+	     //work on some Linux implementations
+	     try {
+	     if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+	     if (Runtime.getRuntime().exec(new String[] {
+	      "which", "xdg- open" }).getInputStream().read() != -1) {
+	     Runtime.getRuntime().exec(new String[] { "xdg-open", videoID });
+	     }
+	     } else {
+	          URI uri = new URI(videoID);
+	          java.awt.Desktop.getDesktop().browse(uri);
+	     }      } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
 
 }
