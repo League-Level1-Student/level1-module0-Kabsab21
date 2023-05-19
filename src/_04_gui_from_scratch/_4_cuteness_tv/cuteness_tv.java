97404790cac3528704e655319ca022b6e614a5a8
league <league@imac-18.attlocal.net>
2023-05-19 16:50:46 -0700

    why...

--- src/_04_gui_from_scratch/_4_cuteness_tv/cuteness_tv.java
+++ src/_04_gui_from_scratch/_4_cuteness_tv/cuteness_tv.java
@@ -1,16 +1,37 @@
 package _04_gui_from_scratch._4_cuteness_tv;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
 
-public class cuteness_tv {
-	public static void main(String[] args) implements ActionListener {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class cuteness_tv implements ActionListener  {
+	JFrame murderme = new JFrame();
+	JPanel why = new JPanel();
+	JButton uhmcduckie = new JButton();
+	JButton uhmcfrogie = new JButton();
+	JButton uhmcunicornie = new JButton();
+	
+	public static void main(String[] args) {
 		cuteness_tv runner = new cuteness_tv();
+		runner.buttons();
 		
 	}
 	
 	
 	private void buttons() {
-		
+		why.add(uhmcduckie);
+		why.add(uhmcfrogie);
+		why.add(uhmcunicornie);
+		murderme.add(why);
+		murderme.setVisible(true);
+		murderme.pack();
+	    uhmcduckie.addActionListener(this);
+	    uhmcfrogie.addActionListener(this);
+	    uhmcunicornie.addActionListener(this);
 	}
 	
 	
@@ -46,5 +67,20 @@ public class cuteness_tv {
 	          e.printStackTrace();
 	     }
 	}
-
+	public void actionPerformed(ActionEvent e) {
+		JButton buttonPressed = (JButton) e.getSource();	
+		if(buttonPressed == uhmcduckie ){
+			System.out.println("uhm McDuckie has been pressed");
+			showDucks();
+		}
+		if(buttonPressed == uhmcfrogie ){
+			System.out.println("uhm McFrogie has been pressed");
+			showFrog();
+		}
+		if(buttonPressed == uhmcunicornie ){
+			System.out.println("uhm McUnicornie has been pressed");
+			showFluffyUnicorns();
+}
+		
+	}
 }
